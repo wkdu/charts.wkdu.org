81c341b593dbc310d361b1252abaf1b5b8333523
Peter Liu <me@peterliu.io>
2018-04-21 21:54:09 -0400

    add bootstrap, remove initial react material, add chart components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import WeeklyCharts from './WeeklyCharts'
+import YearlyCharts from './YearlyCharts'
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main className="container">
+          <div className="row mb-3">
+            <div className="col-lg-12">
+              <WeeklyCharts></WeeklyCharts>
+            </div>
+          </div>
+
+          <div className="row mb-3">
+            <div className="col-lg-12">
+              <YearlyCharts></YearlyCharts>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
+
 }
 
 export default App;
